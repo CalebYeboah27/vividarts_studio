57690f37cd45e6dc793116641f8f39d712d0a147
calebyeboah <calebyeboah27@gmail.com>
2024-04-12 12:36:58 +0000

    update to fetch data from api

--- client/static/js/gallery.js
+++ client/static/js/gallery.js
@@ -13,35 +13,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const s3 = new AWS.S3();
 
-
   const processedImageBucket = "processed-images-vividarts-cy";
-  // Function to fetch images from S3 bucket
+  // Function to fetch images from api
   async function fetchImagesFromS3() {
     try {
       // Populate images in the gallery
 
-      s3.listObjectsV2({ Bucket: processedImageBucket }, (err, data) => {
-        if (err) {
-          console.error("Error fetching data...", err);
-        } else {
-          // Iterate over data being returned to display images
+      const apiUrl = "https://vividarts-api.onrender.com/api/v1/images";
+      const { data } = await fetchData(apiUrl);
 
-          data.Contents.forEach((file) => {
-            const extArr = [".jpg", ".png", ".jpeg"];
-            for (const ext in extArr) {
-              if (file.Key.endsWith(extArr[ext])) {
-                const imageUrl = s3.getSignedUrl("getObject", {
-                  Bucket: processedImageBucket,
-                  Key: file.Key,
-                });
-                const image = document.createElement("img");
-                image.src = imageUrl;
-                gallery.appendChild(image);
-              }
-            }
-          });
-        }
+      data.forEach(({ Key, signedUrl }) => {
+        const alt = Key.split(".jpg")[0];
+        const image = document.createElement("img");
+        image.src = signedUrl;
+        image.alt = alt;
+        gallery.appendChild(image);
       });
+
+      // s3.listObjectsV2({ Bucket: processedImageBucket }, (err, data) => {
+      //   if (err) {
+      //     console.error("Error fetching data...", err);
+      //   } else {
+      //     // Iterate over data being returned to display images
+
+      //     data.Contents.forEach((file) => {
+      //       const extArr = [".jpg", ".png", ".jpeg"];
+      //       for (const ext in extArr) {
+      //         if (file.Key.endsWith(extArr[ext])) {
+      //           const imageUrl = s3.getSignedUrl("getObject", {
+      //             Bucket: processedImageBucket,
+      //             Key: file.Key,
+      //           });
+      //           const image = document.createElement("img");
+      //           image.src = imageUrl;
+      //           gallery.appendChild(image);
+      //         }
+      //       }
+      //     });
+      //   }
+      // });
     } catch (error) {
       console.error("Error fetching images:", error);
     }
